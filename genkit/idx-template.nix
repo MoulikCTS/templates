7ce1e539b7e33e3c45c04a1769e0de3375d9c222
moulik.khamaru <moulik.khamaru@cognizant.com>
2025-12-22 13:00:54 +0000

    first push

--- genkit/idx-template.nix
+++ genkit/idx-template.nix
@@ -10,20 +10,21 @@ idx-template \
     pkgs.go
   ];
   bootstrap = ''
-    mkdir "$out"
-    mkdir "$out"/.idx
-    cp -r ${./.idx}/. "$out/.idx/"
-    cp -f ${./package.json} "$out/package.json"
-    cp -f ${./package-lock.json} "$out/package-lock.json"
-    cp -f ${./index.ts} "$out/index.ts"
-    cp -f ${./.gitignore} "$out/.gitignore"
-    cp ${./README.md} "$out"/README.md
-    ${if language == "go" then "env -C \"$out\" go mod init genkit" else "" }
-    
-    chmod -R u+w "$out"
-    cp ${./.idx/airules.md} "$out/.idx/airules.md"
-    cp ${./.idx/airules.md} "$out/GEMINI.md"
-    chmod -R u+w "$out"
-    
-  '';
+  mkdir "$out"
+  mkdir "$out"/.idx
+  cp -r ${./.idx}/. "$out/.idx/"
+  cp -f ${./package.json} "$out/package.json"
+  cp -f ${./package-lock.json} "$out/package-lock.json"
+  cp -f ${./index.ts} "$out/index.ts"
+  cp -f ${./schema.ts} "$out/schema.ts"
+  cp -f ${./tools.ts} "$out/tools.ts"
+  cp -f ${./.gitignore} "$out/.gitignore"
+  cp ${./README.md} "$out"/README.md
+  ${if language == "go" then "env -C \"$out\" go mod init genkit" else "" }
+  
+  chmod -R u+w "$out"
+  cp ${./.idx/airules.md} "$out/.idx/airules.md"
+  cp ${./.idx/airules.md} "$out/GEMINI.md"
+  chmod -R u+w "$out"
+'';
 }
